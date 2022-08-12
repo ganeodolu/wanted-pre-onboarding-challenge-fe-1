f739be96305e698d113d8d11c04d488ee3e57b88
ganeodolu <ganeodolu@gmail.com>
2022-08-12 20:30:11 +0900

    feat: 브랜치 분리

---
+++ client/src/pages/TodoList.tsx
@@ -0,0 +1,25 @@
+import { Input } from 'elements'
+import React from 'react'
+
+interface Props {
+  
+}
+
+const TodoList = (props: Props) => {
+  return (
+    <>
+    <div style={{ padding: "16px" }}>
+      <h1>Todo List</h1>
+      {/* <Input
+        name="new-todo"
+
+      ></Input> */}
+    </div>
+    <section>
+
+    </section>
+    </>
+  )
+}
+
+export default TodoList
