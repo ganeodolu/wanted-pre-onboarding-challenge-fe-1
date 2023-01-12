2584362fab8bac9f692fe82c4462019ab0b813a7
ganeodolu <ganeodolu@gmail.com>
2023-01-12 13:10:26 +0900

    refactor: axios 기본 설정 통합

--- client/src/lib/api/main.ts
+++ client/src/lib/api/main.ts
@@ -4,6 +4,7 @@ const API = axios.create({
   baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
+    "Authorization": `${localStorage.getItem('user')} ?? undefined`,
   },
 })
 
