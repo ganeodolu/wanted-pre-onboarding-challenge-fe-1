120e4acff233a5d3a29a89ff3862c19c60487188
ganeodolu <ganeodolu@gmail.com>
2023-01-12 13:15:38 +0900

    refactor: placeholder 수정

--- client/src/components/TodoDetail.tsx
+++ client/src/components/TodoDetail.tsx
@@ -10,10 +10,14 @@ const TodoDetail = (props: Props) => {
     <div>
       <h2>Todo Detail</h2>
       <section>
-        <Input
-          name="todo"
+      <Input
+          name="title"
           placeholderText='할일을'
-        >Todo</Input>
+        >Title</Input>
+        <Input
+          name="content"
+          placeholderText='내용을'
+        >Content</Input>
         <Button>추가/수정</Button>
         <Button>삭제</Button>
       </section>
